235175602e6c5967d242ab4799f0f8315534cd29
Lorenzo <lorenzoboda560@gmail.com>
2025-02-17 13:10:32 +0100

    Primer commit

---
+++ src/test/java/com/lbodaszsservidor/registroporpasos/RegistroPorPasosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbodaszsservidor.registroporpasos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroPorPasosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
