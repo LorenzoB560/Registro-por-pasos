31daaaf804a5b238e5fb8cc579a1bb3731ba33d5
Lorenzo <lorenzoboda560@gmail.com>
2025-02-18 14:06:03 +0100

    Cambios

---
+++ src/main/java/com/lbodaszsservidor/registroporpasos/Coleccion.java
@@ -0,0 +1,23 @@
+package com.lbodaszsservidor.registroporpasos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Coleccion {
+
+    private String nombre;
+    private String apellido;
+    /*
+    private LocalDate fechaNacimiento;
+    private String genero;
+    private ArrayList<String> nacionalidades;*/
+
+
+}
--- src/main/java/com/lbodaszsservidor/registroporpasos/Controller/PrincipalController.java
+++ src/main/java/com/lbodaszsservidor/registroporpasos/Controller/PrincipalController.java
@@ -1,25 +1,41 @@
 package com.lbodaszsservidor.registroporpasos.Controller;
 
+import com.lbodaszsservidor.registroporpasos.Coleccion;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class PrincipalController {
 
+
+
     @GetMapping("/datos-personales")
-    public String datosPersonales() {
+    public String datosPersonales(@ModelAttribute Coleccion coleccion, HttpSession sesion, Model model) {
+
+        Object v = sesion.getAttribute ("personales");
+        if (v != null && v.equals(1)){
+            coleccion = (Coleccion) sesion.getAttribute("personales");
+            sesion.setAttribute("personales", coleccion);
+        }
+
+
         return "datos-personales";
     }
     @GetMapping("/datos-profesionales")
-    public String datosProfesionales() {
+    public String datosProfesionales(@ModelAttribute Coleccion coleccion, HttpSession sesion, Model model) {
+
+        sesion.setAttribute("personales", coleccion);
         return "datos-profesionales";
     }
     @GetMapping("/datos-bancarios")
-    public String datosBancarios() {
+    public String datosBancarios(@ModelAttribute Coleccion coleccion, HttpSession sesion, Model model) {
         return "datos-bancarios";
     }
     @GetMapping("/resumen")
-    public String resumen() {
+    public String resumen(@ModelAttribute Coleccion coleccion, HttpSession sesion, Model model) {
         return "resumen";
     }
 }
