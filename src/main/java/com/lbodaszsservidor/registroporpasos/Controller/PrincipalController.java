6ecd86001c4131762758abb1539632d2fdf45e54
Lorenzo <lorenzoboda560@gmail.com>
2025-02-20 11:02:58 +0100

    Realizado el repintado de todos los pasos correctamente

--- src/main/java/com/lbodaszsservidor/registroporpasos/Controller/PrincipalController.java
+++ src/main/java/com/lbodaszsservidor/registroporpasos/Controller/PrincipalController.java
@@ -18,59 +18,112 @@ public class PrincipalController {
     public String datosPersonales(HttpSession sesion, Model model) {
 
         //Obtengo la sesión de personales
-        DatosFormulario datosFormulario = (DatosFormulario) sesion.getAttribute("personales");
+        DatosFormulario datosFormulario = (DatosFormulario) sesion.getAttribute("datos");
 
         // Si no existe, creo el objeto de datos formulario y su sesión respectivamente.
         if(datosFormulario == null) {
+            System.out.println("No sesión datos personales");
             datosFormulario = new DatosFormulario();
-            sesion.setAttribute("personales", datosFormulario);
+            sesion.setAttribute("datos", datosFormulario);
         }
 
         //Lo añado al modelo
-        model.addAttribute("personales", datosFormulario);
-        System.out.println(sesion.getAttribute("personales"));
+        model.addAttribute("datos", datosFormulario);
+        System.out.println(sesion.getAttribute("datos"));
 
         return "datos-personales";
     }
+    @PostMapping("/guardar-datos-personales")
+    public String guardarDatosPersonales(@ModelAttribute DatosFormulario datosFormulario, HttpSession sesion) {
+        sesion.setAttribute("datos", datosFormulario);
+        return "redirect:/datos-profesionales";
+    }
+
     @GetMapping("/datos-profesionales")
     public String datosProfesionales(HttpSession sesion, Model model) {
 
-        DatosFormulario datosFormulario = (DatosFormulario) sesion.getAttribute("personales");
+        DatosFormulario datosFormulario = (DatosFormulario) sesion.getAttribute("datos");
+        System.out.println(datosFormulario);
         if(datosFormulario == null) {
-            System.out.println("No sesion");
+            System.out.println("No sesión datos profesionales");
             datosFormulario = new DatosFormulario();
-            sesion.setAttribute("personales", datosFormulario);
+            sesion.setAttribute("datos", datosFormulario);
         }
-        model.addAttribute("personales", datosFormulario);
-
+        model.addAttribute("datos", datosFormulario);
         return "datos-profesionales";
     }
     @PostMapping("/guardar-datos-profesionales")
     public String guardarDatosProfesionales(@ModelAttribute DatosFormulario datosFormulario, HttpSession sesion) {
-        sesion.setAttribute("personales", datosFormulario);
-        return "redirect:/datos-profesionales";
+
+        DatosFormulario datosAnteriores = (DatosFormulario) sesion.getAttribute("datos");
+
+        actualizarDatos(datosAnteriores, datosFormulario);
+
+        sesion.setAttribute("datos", datosFormulario);
+        return "redirect:/datos-bancarios";
     }
 
 
     @GetMapping("/datos-bancarios")
     public String datosBancarios(HttpSession sesion, Model model) {
 
-        DatosFormulario datosFormulario = (DatosFormulario) sesion.getAttribute("profesionales");
+        DatosFormulario datosFormulario = (DatosFormulario) sesion.getAttribute("datos");
+        System.out.println(datosFormulario);
         if(datosFormulario == null) {
+            System.out.println("No sesión datos bancarios");
             datosFormulario = new DatosFormulario();
-            sesion.setAttribute("profesionales", datosFormulario);
+            sesion.setAttribute("datos", datosFormulario);
         }
-        model.addAttribute("profesionales", datosFormulario);
+        model.addAttribute("datos", datosFormulario);
         return "datos-bancarios";
     }
     @PostMapping("/guardar-datos-bancarios")
     public String guardarDatosBancarios(@ModelAttribute DatosFormulario datosFormulario, HttpSession sesion) {
-        sesion.setAttribute("profesionales", datosFormulario);
-        return "redirect:/datos-bancarios";
+
+        DatosFormulario datosAnteriores = (DatosFormulario) sesion.getAttribute("datos");
+
+        actualizarDatos(datosAnteriores, datosFormulario);
+
+        sesion.setAttribute("datos", datosFormulario);
+        return "redirect:/resumen";
     }
 
     @GetMapping("/resumen")
-    public String resumen(@ModelAttribute DatosFormulario datosFormulario, HttpSession sesion, Model model) {
-        return "resumen";
+    public String resumen(HttpSession sesion, Model model) {
+        DatosFormulario datosFormulario = (DatosFormulario) sesion.getAttribute("datos");
+
+        if (datosFormulario == null) {
+            System.out.println("No sesión resumen");
+            datosFormulario = new DatosFormulario();
+        }
+
+        model.addAttribute("datos", datosFormulario);
+        System.out.println(sesion.getAttribute("datos"));
+        return "resumen";  // No vuelvas a guardar en la sesión
+    }
+
+
+    private void actualizarDatos(DatosFormulario sesionVieja, DatosFormulario sesionNueva) {
+        if (sesionVieja.getNombre() != null){
+            sesionNueva.setNombre(sesionVieja.getNombre());
+        }
+        if (sesionVieja.getApellido() != null){
+            sesionNueva.setApellido(sesionVieja.getApellido());
+        }
+        if (sesionVieja.getDepartamento() != null){
+            sesionNueva.setDepartamento(sesionVieja.getDepartamento());
+        }
+        if (sesionVieja.getSalario() > 0){
+            sesionNueva.setSalario(sesionVieja.getSalario());
+        }
+        if (sesionVieja.getBancoNombre() != null) {
+            sesionNueva.setBancoNombre(sesionVieja.getBancoNombre());
+        }
+
+        if (sesionVieja.getCuentaBancaria() > 0){
+            sesionNueva.setCuentaBancaria(sesionVieja.getCuentaBancaria());
+        }
+
+
     }
 }
--- src/main/java/com/lbodaszsservidor/registroporpasos/model/DatosFormulario.java
+++ src/main/java/com/lbodaszsservidor/registroporpasos/model/DatosFormulario.java
@@ -1,23 +1,32 @@
 package com.lbodaszsservidor.registroporpasos.model;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class DatosFormulario {
 
+
+
     private String nombre;
     private String apellido;
-    /*
     private LocalDate fechaNacimiento;
     private String genero;
-    private ArrayList<String> nacionalidades;*/
+    private ArrayList<String> nacionalidades;
 
 
     private String departamento;
     private double salario;
+
+    private String bancoNombre;
+    private int cuentaBancaria;
 
 }
